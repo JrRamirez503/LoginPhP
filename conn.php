65d41a86baa238900165c0b6d2a5ccd6c5660336
JrRamirez503 <54161271+JrRamirez503@users.noreply.github.com>
2019-12-06 19:32:03 -0600

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,7 @@
+<?php
+// Connection variables
+$dbhost	= "localhost";	   // localhost or IP
+$dbuser	= "josue";		  // database username
+$dbpass	= "1234";		     // database password
+$dbname	= "loginphp";    // database name
+?>
